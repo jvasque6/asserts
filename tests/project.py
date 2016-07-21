eb27cb73a521beff467d4fc38f922d70ef827c29
Rafael Alvarez <ralvarez@fluid.la>
2016-07-21 18:36:21 -0500

    * Comentar el uso de http y cookies en el proyecto pues Yerman la cago, * Adicionar notas al README explicando estructura del API
    Former-commit-id: cf60052181e92ad73a8daa16d56bc91c0d03ef8d

--- tests/project.py
+++ tests/project.py
@@ -1,6 +1,6 @@
 from fluidasserts import pdf
-from fluidasserts import http
-from fluidasserts import cookie
+#from fluidasserts import http
+#from fluidasserts import cookie
 
 pdf.has_author('tests/data/vulnerable.pdf')
 pdf.has_creator('tests/data/vulnerable.pdf')
@@ -41,6 +41,56 @@ pdf.has_producer('tests/data/non-vulnerable.pdf')
 #http.has_header_access_control_allow_origin("http://localhost/cursos")
 #http.has_header_access_control_allow_origin("http://challengeland.co")
 
-cookie.has_http_only("http://challengeland.co","ci_session")
+#cookie.has_http_only("http://challengeland.co","ci_session")
 #http.basic_auth("http://localhost/fluidopens/BasicAuth/","root","1234")
 #http.basic_auth("http://localhost/fluidopens/BasicAuth/","Admin","1234")
+
+
+# javascript module
+#js.is_obfuscated('Admin.js')
+
+# html module
+# using elementtree 
+# http://stackoverflow.com/questions/8692/how-to-use-xpath-in-python 
+# http://effbot.org/zone/element-xpath.htm
+#html.no_autocomplete('Archivo.html', 'dom.location.dsdf.sd.fdf')
+
+# ssl module or x509 module?
+# using twisted
+# http://stackoverflow.com/questions/1087227/validate-ssl-certificates-with-python
+# http://stackoverflow.com/questions/16899247/how-can-i-decode-a-ssl-certificate-using-python
+# http://pyopenssl.sourceforge.net/pyOpenSSL.html/openssl-x509.html
+# https://www.sslshopper.com/ssl-checker.html#hostname=https://fluid.la
+# ssl.is_self_signed()
+# ssl.has_expired()
+# ssl.expires_soon(asdsd, days)
+# ssl.resolved_accordinly 
+
+# http module
+# pruebas asimetricas
+# http.response_is_stable(seconds, URL, repeat)
+
+# tcp.is_open(ip, port)
+
+# ssh module
+# ssh.has_advisory()
+# ssh.is_open()
+# ssh.
+
+# ldap module
+# ldap.is_open(ip)
+# ldap.supports_anonymous_connection()
+
+# ftp module
+# ftp.is_open(ip)
+# ftp.supports_anonymous_connection(ip)
+
+# smtp module
+# smtp.is_open()
+# smtp.supports_anonymous_connection(ip)
+
+# dns module
+# dns.is_open()
+# dns.zone_transfer()
+
+# voip??
