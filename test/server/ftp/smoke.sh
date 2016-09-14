1351f0fc705c3b11bb3ed70747b5f19ebe86e884
Rafael Alvarez <ralvarez@fluid.la>
2016-09-14 17:39:28 -0500

    Transforma mock de FTP de docker a ansible La configuración ahora es dinamica, entre el FTP original, el endurecido (hard) y el debilitado (weak)
    Solo se necesita un container al tiempo, no dos como antes.
    
    La configuración esta expresada como codigo, por ende tecnicamente
    esta programado el endurecimiento.
    
    La misma suite de pruebas decida cuando endurecer o debilitar.
    
    
    Former-commit-id: 8a2f707c014158b78e0fbddd76aa3432fad05dce

--- test/server/ftp/smoke.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-# shellcheck disable=SC1091
-source conf.sh
-
- lftp -c "set ftp:passive-mode yes; open -u root,root123 $VULNERABLE_IP; ls -al"
- lftp -c "set ftp:passive-mode yes; open -u root,root123 $HARDENED_IP; ls -al"
- lftp -c "set ftp:passive-mode no; open -u root,root123 $VULNERABLE_IP; ls -al"
- lftp -c "set ftp:passive-mode no; open -u root,root123 $HARDENED_IP; ls -al"
