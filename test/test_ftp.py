4c0f1738b9d9b5c384fc9c3dbc99cfb5ea9004d5
Rafael Alvarez <ralvarez@fluid.la>
2016-09-14 17:51:20 -0500

    Centralización de la prueba de ftp con nuevo modelo Ahora solo se usa un contenedor no varios.
    Ahora la misma suite de pruebas hace configuración de endurecimiento
    o debilitamiento a partir de fixtures.
    
    Ahora se invoca solo una vez la configuración correspondiente y no
    una vez por cada función (scope).
    
    Debido a esto la prueba que se hace siempre usa el mismo set de datos
    solo que sobre un container reconfigurado (endurecido o debilitado).
    
    
    Former-commit-id: 6297ad92e02a80c6e3d7d344ad8af6bf33b4ffef

--- test/test_ftp.py
+++ test/test_ftp.py
@@ -12,71 +12,102 @@ VSFTP
 """
 
 # standard imports
-# none
+import subprocess
 
 # 3rd party imports
-# none
+import pytest
 
 # local imports
 from fluidasserts import ftp
 
-IP_HARDENED = '10.82.21.77'
-IP_VULNERABLE = '10.82.21.66'
 
-ADMIN_PASSWORD = 'root123'
-NONPASS_USERNAME = 'dario'
-SECURED_USERNAME = 'freddy'
-GUESSED_USERNAME = 'faustino'
-GUESSED_PASSWORD = 'faustino123'
-CHANGED_SUFFIX = 'CHANGED'
+#
+# Constants
+#
+
+# TODO(ralvarez): Pueden ser cargadas del hosts ini style
+CONTAINER_IP = '172.18.1.1'
+ADMIN_PASS = 'ahViQu9E'
+NONPASS_USER = 'nonpass'
+SECURED_USER = 'secured'
+GUESSED_USER = 'guessed'
+GUESSED_PASS = 'guessed123'
 
 
+#
+# Fixtures
+#
+
+
+@pytest.fixture(scope='module')
+def weak_ftp(request):
+    """Configura perfil de FTP vulnerable."""
+    print('Running FTP vulnerable playbook')
+    subprocess.call('ansible-playbook test/setup/ftp.yml \
+                                      --tags weak', shell=True)
+
+
+@pytest.fixture(scope='module')
+def hard_ftp(request):
+    """Configura perfil de FTP endurecido."""
+    print('Running FTP hardened playbook')
+    subprocess.call('ansible-playbook test/setup/ftp.yml \
+                                      --tags hard', shell=True)
+
+
+#
+# Open tests
+#
+
+
+@pytest.mark.usefixtures('container', 'weak_ftp')
 def test_is_anonymous_enabled_open():
     """Servidor FTP vulnerable SI soporta conexion anonima?"""
-    assert ftp.is_anonymous_enabled(IP_VULNERABLE)
+    assert ftp.is_anonymous_enabled(CONTAINER_IP)
 
 
-def test_is_anonymous_enabled_close():
-    """Servidor FTP endurecido NO soporta conexion anonima?"""
-    assert not ftp.is_anonymous_enabled(IP_HARDENED)
-
-
+@pytest.mark.usefixtures('container', 'weak_ftp')
 def test_is_admin_enabled_open():
     """Servidor FTP vulnerable SI soporta conexion del ADMIN"""
-    assert ftp.is_admin_enabled(IP_VULNERABLE,
-                                ADMIN_PASSWORD)
+    assert ftp.is_admin_enabled(CONTAINER_IP, ADMIN_PASS)
 
 
-def test_is_admin_enabled_close():
-    """Servidor FTP endurecido NO soporta conexion del ADMIN?"""
-    # TODO(ralvarez) Idealmente mismas credenciales en HARDENED
-    assert not ftp.is_admin_enabled(IP_HARDENED,
-                                    (ADMIN_PASSWORD + CHANGED_SUFFIX))
-
-
+@pytest.mark.usefixtures('container', 'weak_ftp')
 def test_user_without_password_open():
     """Servidor FTP vulnerable SI autentica usuario sin clave?"""
-    assert ftp.user_without_password(IP_VULNERABLE,
-                                     NONPASS_USERNAME)
+    assert ftp.user_without_password(CONTAINER_IP, NONPASS_USER)
 
 
-def test_user_without_password_close():
-    """Servidor FTP endurecido NO autentica usuario sin clave"""
-    # TODO(ralvarez) Idealmente mismas credenciales en HARDENED
-    assert not ftp.user_without_password(IP_HARDENED,
-                                         SECURED_USERNAME)
-
-
+@pytest.mark.usefixtures('container', 'weak_ftp')
 def test_is_a_valid_user_open():
     """Servidor FTP vulnerable SI autentica a usuario adivinado?"""
-    assert ftp.is_a_valid_user(IP_VULNERABLE,
-                               GUESSED_USERNAME,
-                               GUESSED_PASSWORD)
+    assert ftp.is_a_valid_user(CONTAINER_IP, GUESSED_USER, GUESSED_PASS)
 
 
+#
+# Closing tests
+#
+
+
+@pytest.mark.usefixtures('container', 'hard_ftp')
+def test_is_anonymous_enabled_close():
+    """Servidor FTP endurecido NO soporta conexion anonima?"""
+    assert not ftp.is_anonymous_enabled(CONTAINER_IP)
+
+
+@pytest.mark.usefixtures('container', 'hard_ftp')
+def test_is_admin_enabled_close():
+    """Servidor FTP endurecido NO soporta conexion del ADMIN?"""
+    assert not ftp.is_admin_enabled(CONTAINER_IP, ADMIN_PASS)
+
+
+@pytest.mark.usefixtures('container', 'hard_ftp')
+def test_user_without_password_close():
+    """Servidor FTP endurecido NO autentica usuario sin clave"""
+    assert not ftp.user_without_password(CONTAINER_IP, NONPASS_USER)
+
+
+@pytest.mark.usefixtures('container', 'hard_ftp')
 def test_is_a_valid_user_close():
     """Servidor FTP endurecido NO autentica a usuario adivinado?"""
-    # TODO(ralvarez) Idealmente mismas credenciales en HARDENED
-    assert not ftp.is_a_valid_user(IP_HARDENED,
-                                   GUESSED_USERNAME,
-                                   (GUESSED_PASSWORD + CHANGED_SUFFIX))
+    assert not ftp.is_a_valid_user(CONTAINER_IP, GUESSED_USER, GUESSED_PASS)
