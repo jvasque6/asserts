a808bf76027604a97d503ec610585761a3b5c934
Rafael Alvarez <ralvarez@fluid.la>
2016-09-16 16:52:34 -0500

    Generación automatica de llaves SSH
    Former-commit-id: 7c32fe0bffff43b8fa8cd1305dba8c63f4058a2e

--- test/container/start.sh
+++ test/container/start.sh
@@ -17,6 +17,14 @@ docker network create \
 		--subnet="$NET_IP" \
 		"$NET_NAME"
 
+# En cada inicio se configura la conexión SSH correspondiente
+# Este archivo de configuración de SSH tambien esta parametrizado
+# en setup/config sección ssh, parametro ssh_args
+export PROJECT_PATH="${ANSIBLE_CONFIG%/*/*/*}"
+cp "$PROJECT_PATH"/test/setup/ssh_config ~/.ssh/config.facont
+echo -e "y\n" | ssh-keygen -b 2048 -t rsa -f ~/.ssh/facont_id_rsa -q -N ""
+#test -f ~/.ssh/facont_id_rsa || ssh-keygen -q -N "" -t rsa -b 2048 -f ~/.ssh/facont_id_rsa
+
 docker run \
 		--detach \
 		--name="$SERVICE" \
@@ -25,5 +33,5 @@ docker run \
 		--ip "$IP" \
 		--publish-all \
 		--volume=/tmp:/host/tmp \
-		-e SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" \
+		-e SSH_KEY="$(cat ~/.ssh/facont_id_rsa.pub)" \
 		fluidsignal/"$SERVICE"
