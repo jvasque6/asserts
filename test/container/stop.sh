e0affe223377c83e69f35dd658c0428db6462821
Rafael Alvarez <ralvarez@fluid.la>
2016-09-18 23:30:56 -0500

    Fix: Smoke test con reuso de red, exportar variables para debug y CI
    Former-commit-id: 302100ae5c6d1e16e6891d11cb4fa165e1126234

--- test/container/stop.sh
+++ test/container/stop.sh
@@ -17,6 +17,7 @@ if [ -z $(docker ps -q -f name="$SERVICE") ]; then
 elif [ -n "$CIRCLECI" ]; then
   # este es un workaround debido a que en CIRCLECI no hay docker rm/kill
   echo "Contenedor prendido, pero en CIRCLECI, reutilizandolo."	 
+  CONTAINER_ACTIVE="yes"
 else
   echo "Contenedor prendido, y no en CIRCLECI, deteniendolo..."
   docker kill "$SERVICE"
@@ -26,8 +27,10 @@ fi
 # eliminar red de contenedores si esta establecida
 if [ -z $(docker network ls -q -f name="$NET_NAME") ]; then
   echo "Red ya eliminada..."
+elif [ -n "$CONTAINER_ACTIVE" ]; then
+  echo "Contenedores aun prendidos, mantener red prendida..."
 else
-  echo "Red configurada, eliminandola..."
+  echo "Red configurada y sin contenedores activos, eliminandola..."
   docker network rm fluidasserts
 fi
 
