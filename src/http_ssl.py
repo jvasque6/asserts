f39b1834ed096f166b572fcb0cb9818dc0f0429b
Andrés Roldán <aroldan@fluid.la>
2016-10-07 15:05:57 -0500

    Implemented http_ssl.is_cert_active that checks whether a cert is still valid (not expired)
    Former-commit-id: 64226456935b3ee857bc311489360be1d83bf830

--- src/http_ssl.py
+++ src/http_ssl.py
@@ -9,6 +9,7 @@ import socket
 
 # 3rd party imports
 import ssl
+import datetime
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.x509 import load_pem_x509_certificate
@@ -38,6 +39,29 @@ def is_cert_cn_equal_to_site(site, port=PORT):
         logging.info('%s CN equals to site, Details=%s, %s',
                      cert_cn, site, 'CLOSE')
         result = False
+    return result
+
+
+def is_cert_active(site, port=PORT):
+    """
+    Function to check whether cert is still valid
+    """
+    result = True
+    cert = str(ssl.get_server_certificate((site, port)))
+    cert_obj = load_pem_x509_certificate(cert, default_backend())
+
+    if cert_obj.not_valid_after > datetime.datetime.now():
+        logging.info('Certificate is still valid, Details=Not valid\
+                      after: %s, Current time: %s, %s',
+                     cert_obj.not_valid_after.isoformat(),
+                     datetime.datetime.now().isoformat(), 'CLOSE')
+        result = False
+    else:
+        logging.info('Certificate is not valid, Details=Not valid\
+                      after: %s, Current time: %s, %s',
+                     cert_obj.not_valid_after.isoformat(),
+                     datetime.datetime.now().isoformat(), 'OPEN')
+        result = True
     return result
 
 
