3c2e0106ff3f8ac002b84737e3c6d78c1e717ef8
Andrés Roldán <aroldan@fluid.la>
2016-12-29 16:17:56 -0500

    Fixed tests errors.
    Former-commit-id: e3b57d1f55f606e1255c58481f4477810c54fcd9

--- src/__init__.py
+++ src/__init__.py
@@ -16,12 +16,15 @@ from validate import Validator
 # local imports
 # none
 
+try:
+    _log_config_file = 'conf.cfg'
+    _log_config_location = resource_stream(__name__, _log_config_file)
 
-_log_config_file = 'conf.cfg'
-_log_config_location = resource_stream(__name__, _log_config_file)
-
-_log_spec_file = 'conf.spec'
-_log_spec_location = resource_stream(__name__, _log_spec_file)
+    _log_spec_file = 'conf.spec'
+    _log_spec_location = resource_stream(__name__, _log_spec_file)
+except:
+    _log_config_location = 'conf/conf.cfg'
+    _log_spec_location = 'conf/conf.spec'
 
 # pylint: disable=C0103
 cfg = ConfigObj(_log_config_location, configspec=_log_spec_location)
