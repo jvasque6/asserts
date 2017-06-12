eb0f50bfbff94994dc75e1bdb23745009731e7e4
Andres Roldan <aroldan@fluid.la>
2017-06-12 12:27:52 -0500

    More work on smb module. Now the code is tested locally. Pending upload of samba ansible playbook.
    Former-commit-id: 3b76fd50bd55958b96f2b0d36853cc3fb24ae61e

--- src/service/smb.py
+++ src/service/smb.py
@@ -4,10 +4,11 @@
 # standard imports
 from __future__ import absolute_import
 import logging
+import socket
 
 # 3rd party imports
+from smb import SMBConnection
 from smb import *
-from SMBConnection import *
 
 # local imports
 from fluidasserts import show_close
@@ -16,45 +17,57 @@ from fluidasserts import show_open
 logger = logging.getLogger('FLUIDAsserts')
 
 
-def __smb_connect(server=server, user=user, password=password,
-                  domain=domain):
+def __smb_connect(server=None, user=None, password=None,
+                  domain='WORKGROUP'):
     """Returns a SMB connection handler."""
     try:
         client_machine_name = 'assertspc'
-        conn = SMBConnection(user password, client_machine_name, server,
-            domain=domain, use_ntlm_v2=True, is_direct_tcp=True)
+        conn = SMBConnection.SMBConnection(user, password,
+            client_machine_name, server, domain=domain, use_ntlm_v2=True,
+            is_direct_tcp=True)
 
-        return conn.connect(server, 445)
-    except:
-        return None
+        if conn.connect(server, 445):
+            return conn
+        return False
+    except socket.error:
+        return False
 
 
-def has_dirlisting(share=share, *args, **kwargs):
+def has_dirlisting(share=None, *args, **kwargs):
     """Check if a SMB share has dirlisting."""
     conn = __smb_connect(*args, **kwargs)
 
-    if conn is False:
+    if not conn:
+        logger.info('%s: Error while connecting, \
+Details=%s\%s:%s', show_open('ERROR'), domain, user, server)
+
+        return False
+
+    try:
+        sharedfiles = conn.listPath(share, '/')
+        logger.info('%s: Directory listing is possible, \
+Details=%s\%s:%s', show_open(), domain, user, server)
+
+        return True
+    except smb_structs.OperationFailure:
         logger.info('%s: Directory listing not possible, \
 Details=%s\%s:%s', show_close(), domain, user, server)
 
         return False
 
-    sharedfiles = conn.listPath(share, '/')
-    print (sharedfiles)
 
-
-def is_anonymous_enabled(server=server, domain=domain):
+def is_anonymous_enabled(server=None, domain='WORKGROUP'):
     """Check if a SMB share has dirlisting."""
     user = 'anonymous'
     password = ''
     conn = __smb_connect(server=server, user=user, password=password,
         domain=domain)
 
-    if conn is False:
-        logger.info('%s: Anonymous login not possible, \
-Details=%s\%s:%s', show_close(), domain, user, server)
+    if not conn:
+        logger.info('%s: Error while connecting, \
+Details=%s\%s:%s', show_open('ERROR'), domain, user, server)
 
         return False
     logger.info('%s: Anonymous login enabled, Details=%s\%s:%s',
-        show_close(), domain, user, server)
+        show_open(), domain, user, server)
     return True
