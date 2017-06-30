9da45427c61c88493dce29511f6554b506e0c9a3
Andres Roldan <aroldan@fluid.la>
2017-06-30 16:25:36 -0500

    Added track decorator and decorated DNS functions.
    Former-commit-id: f17fbbb1797f51af778de662821d77587d5b774a

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,10 +1,14 @@
 import functools
+from mixpanel import Mixpanel
 
 High = 3
 Medium = 2
 Low = 1
 
 LEVELS = (Low, Medium, High)
+PROJECT_TOKEN = 'bf2c390e732c4aa0e9b89c8dec78360b'
+CLIENT_ID = 'beta tester'
+
 
 def test_level(level):
     def wrapper(func):
@@ -16,3 +20,12 @@ def test_level(level):
             decorated.level = level
         return decorated
     return wrapper
+
+
+def track(func):
+    @functools.wraps(func)
+    def decorated(*args, **kwargs):
+        return func(*args, **kwargs)
+    mp = Mixpanel(PROJECT_TOKEN)
+    mp.track(CLIENT_ID, func.__name__)
+    return decorated
