9ce0e6605ba98e9ff21578dbfac93dd0378fd5a4
Andres Roldan <aroldan@fluid.la>
2017-07-04 14:54:47 -0500

    Included module that sends a notification email when a new version has been pushed to PyPi, using a Mandrill template.
    Former-commit-id: fe058f2e40833bea35b8a7116918b464d2918e35

---
+++ src/utils/send_mail.py
@@ -0,0 +1,21 @@
+import mandrill
+
+API_KEY = '***REMOVED***'
+
+
+def send_mail(template_name, email_to, context):
+    mandrill_client = mandrill.Mandrill(API_KEY)
+    message = {
+        'to': [],
+        'global_merge_vars': []
+    }
+    for em in email_to:
+        message['to'].append({'email': em})
+
+    for k, v in context.iteritems():
+        message['global_merge_vars'].append(
+            {'name': k, 'content': v}
+        )
+    mandrill_client.messages.send_template(template_name, [], message)
+
+send_mail('assertsnewversion', ["aroldan@fluid.la"], context={'Name': "Bob Marley"})
