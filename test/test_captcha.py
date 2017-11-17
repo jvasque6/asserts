6917423893632ec9fe8fc498feaf50dd129038a2
Andres Roldan <aroldan@fluid.la>
2017-11-17 13:36:31 -0500

    Added test/test_captcha.py
    Former-commit-id: 968f0f7433daf7834a7e61348dbb2b294fc03118

---
+++ test/test_captcha.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""Modulo para pruebas de captcha."""
+
+# standard imports
+# None
+
+# 3rd party imports
+# None
+
+# local imports
+from fluidasserts.format import captcha
+
+# Constants
+SECURE_CAPTCHA_IMG = ['test/provision/captcha/secure.jpg','504375']
+WEAK_CAPTCHA_IMG = ['test/provision/captcha/weak.jpg','9T795']
+
+
+#
+# Open tests
+#
+
+
+def test_is_insecure_in_image_open():
+    """Insecure captcha open"""
+    assert captcha.is_insecure_in_image(WEAK_CAPTCHA_IMG[0],
+                                        WEAK_CAPTCHA_IMG[1])
+
+
+#
+# Closing tests
+#
+
+
+def test_is_insecure_in_image_close():
+    """Insecure captcha close"""
+    assert not captcha.is_insecure_in_image(SECURE_CAPTCHA_IMG[0],
+                                            SECURE_CAPTCHA_IMG[1])
+
