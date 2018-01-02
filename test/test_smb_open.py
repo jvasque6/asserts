22d2727e0de219c1b6f0b171fd89307317fbb812
Andres Roldan <aroldan@fluid.la>
2018-01-02 16:05:40 -0500

    Re-enabled SMB tests (just closing tests for now)
    Former-commit-id: 9ec164fac397adc2c42ceace1e735a79c140d7e3

---
+++ test/test_smb_open.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""Modulo para pruebas de SMB.
+
+Este modulo contiene las funciones necesarias para probar si el modulo de
+SMB se encuentra adecuadamente implementado.
+"""
+
+# standard imports
+from __future__ import print_function
+import subprocess
+import time
+
+# 3rd party imports
+import pytest
+
+# local imports
+from fluidasserts.service import smb
+
+# Constants
+CONTAINER_IP = '172.30.216.101'
+SMB_PORT = 445
+
+#
+# Open tests
+#
+
+
+@pytest.mark.parametrize('run_mock',
+                         [('smb:weak', {'445/tcp': SMB_PORT})],
+                         indirect=True)
+def test_is_anonymous_enabled_open(run_mock):
+    """Conexion anonima habilitada?"""
+    time.sleep(5)
+    assert smb.is_anonymous_enabled(CONTAINER_IP)
