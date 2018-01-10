1104e603d581ce2bab2ef70758f812059e9617de
Andres Roldan <aroldan@fluid.la>
2018-01-10 15:59:55 -0500

    Removed lint stage from local-integration.nix
    Former-commit-id: a2e3693834de6261e5c1b1eb18af2b53e02c7022

--- local-integration.nix
+++ local-integration.nix
@@ -9,5 +9,4 @@ DOCKER_PASS="set"
 DOCKER_USER="set"
 
 gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" --env "FLUIDASSERTS_LICENSE_KEY=$FLUIDASSERTS_LICENSE_KEY" --env "FLUIDASSERTS_USER_EMAIL=$FLUIDASSERTS_USER_EMAIL" build
-gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" --env "FLUIDASSERTS_LICENSE_KEY=$FLUIDASSERTS_LICENSE_KEY" --env "FLUIDASSERTS_USER_EMAIL=$FLUIDASSERTS_USER_EMAIL" lint
 gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" --env "FLUIDASSERTS_LICENSE_KEY=$FLUIDASSERTS_LICENSE_KEY" --env "FLUIDASSERTS_USER_EMAIL=$FLUIDASSERTS_USER_EMAIL" test
