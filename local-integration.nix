bc1a6438acdf841d204e4c0f0a408fa8818cf898
Andres Roldan <aroldan@fluid.la>
2018-02-06 11:04:49 -0500

    Use JFROG to store the package
    Changed env variables everywhere
    
    Remove trailing whitespace
    
    
    Former-commit-id: ca0913f3a2b33606a4e25db245fab3727ca87015

--- local-integration.nix
+++ local-integration.nix
@@ -3,10 +3,10 @@
 
 FA_LICENSE_KEY="set"
 FA_USER_EMAIL="set"
-PYPI_USER="set"
-PYPI_PASS="set"
+JFROG_USER="set"
+JFROG_PASS="set"
 DOCKER_PASS="set"
 DOCKER_USER="set"
 
-gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" --env "FA_LICENSE_KEY=$FA_LICENSE_KEY" --env "FA_USER_EMAIL=$FA_USER_EMAIL" build
-gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" --env "FA_LICENSE_KEY=$FA_LICENSE_KEY" --env "FA_USER_EMAIL=$FA_USER_EMAIL" test
+gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "JFROG_USER=$JFROG_USER" --env "JFROG_PASS=$JFROG_PASS" --env "FA_LICENSE_KEY=$FA_LICENSE_KEY" --env "FA_USER_EMAIL=$FA_USER_EMAIL" build
+gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "JFROG_USER=$JFROG_USER" --env "JFROG_PASS=$JFROG_PASS" --env "FA_LICENSE_KEY=$FA_LICENSE_KEY" --env "FA_USER_EMAIL=$FA_USER_EMAIL" test
