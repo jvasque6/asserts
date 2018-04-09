0c9aa1449a9a864b0cc601600cd94d5a5b3988f0
Andres Roldan <aroldan@fluid.la>
2018-04-09 11:23:37 -0500

    Implemented rpgle.has_unitialized_vars()
    Former-commit-id: adf7d7e64fd944a4e35e16ebe111345c03b9ab45

--- src/code/rpgle.py
+++ src/code/rpgle.py
@@ -16,7 +16,8 @@ from fluidasserts.helper import code_helper
 from fluidasserts import show_close
 from fluidasserts import show_open
 from fluidasserts.utils.decorators import track
-from pyparsing import CaselessKeyword, Literal
+from pyparsing import (CaselessKeyword, Literal, Word, Optional, NotAny,
+                       alphas, alphanums, nums)
 
 
 @track
@@ -37,5 +38,33 @@ def has_dos_dow_sqlcod(rpg_dest):
             result = True
         else:
             show_close('Code does not have DoS for using "DoW SQLCOD = 0"',
+                       details='File: {}'.format(code_file))
+    return result
+
+
+@track
+def has_unitialized_vars(rpg_dest):
+    """Search for unitialized variables."""
+    tk_data = Word('D')
+    tk_first = Word(alphas+"_", exact=1)
+    tk_rest = Word(alphanums+"_")
+    tk_vartype = Word(alphas, exact=1)
+    tk_varlen = Word(nums) + Word(alphas, exact=1)
+    tk_inz = CaselessKeyword('inz')
+    tk_varname = tk_first + tk_rest
+
+    unitialized = tk_data + tk_varname + Optional(tk_vartype) + \
+                  tk_varlen + Optional(Word(nums)) + NotAny(tk_inz)
+
+    result = False
+    matches = code_helper.check_grammar(unitialized, rpg_dest, '.rpg')
+    for code_file, vulns in matches.items():
+        if vulns:
+            show_open('Code has unitialized variables',
+                      details='File: {}, Lines: {}'.
+                      format(code_file, ",".join([str(x) for x in vulns])))
+            result = True
+        else:
+            show_close('Code has not unitialized variables',
                        details='File: {}'.format(code_file))
     return result
--- test/test_code_rpgle.py
+++ test/test_code_rpgle.py
@@ -33,6 +33,17 @@ def test_has_dos_dow_sqlcod_in_dir_open():
     """Code has DoS for using DoW SQLCOD = 0."""
     assert rpgle.has_dos_dow_sqlcod(CODE_DIR)
 
+
+def test_has_unitialized_open():
+    """Code has unitialized variables."""
+    assert rpgle.has_unitialized_vars(INSECURE_CODE)
+
+
+def test_has_unitialized_in_dir_open():
+    """Code has unitialized variables."""
+    assert rpgle.has_unitialized_vars(CODE_DIR)
+
+
 #
 # Closing tests
 #
@@ -41,3 +52,8 @@ def test_has_dos_dow_sqlcod_in_dir_open():
 def test_has_dos_dow_sqlcod_close():
     """Code has DoS for using DoW SQLCOD = 0."""
     assert not rpgle.has_dos_dow_sqlcod(SECURE_CODE)
+
+
+def test_has_unitialized_close():
+    """Code has unitialized variables."""
+    assert not rpgle.has_unitialized_vars(SECURE_CODE)
