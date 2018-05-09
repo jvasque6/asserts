a8676b7c5efb4acc07b8ca3371002fde1ef697f9
R Alberto Ballestas <rballestas@fluidattacks.com>
2018-05-09 17:43:27 -0500

    ReSTructure format.html
    Minimal code change:
      if x is False: -> if not x:
    
    
    Former-commit-id: e229c2900bb1d8bb16f19c58baeeb2842ed528cf

--- fluidasserts/format/html.py
+++ fluidasserts/format/html.py
@@ -15,11 +15,27 @@ from fluidasserts.utils.decorators import track
 
 
 def has_attribute(filename, selector, tag, attr, value):
-    """Check attribute value.
+    """
+    Check ``HTML`` attributes` values.
 
     This method checks whether the code retrieved by the selector
-    (selector) inside the file (file) has an attribute (attr) with the
-    specific value (value)
+    (``selector``) inside the file (``filename``)
+    has an attribute (``attr``) with the specific value (``value``).
+
+    All the parameters except the filename can be Python regular expressions.
+
+    :param filename: Path to the ``HTML`` source.
+    :type filename: string
+    :param selector: ``CSS`` selector to test.
+    :type selector: string
+    :param tag: ``HTML`` tag to search.
+    :type tag: string.
+    :param attr: Attribute to search.
+    :type attr: string
+    :param value: Value the attribute should have.
+    :type value: string
+    :rtype: bool
+    :returns: True if attribute set as specified, False otherwise.
     """
     handle = open(filename, 'r')
     html_doc = handle.read()
@@ -38,7 +54,17 @@ def has_attribute(filename, selector, tag, attr, value):
 
 @track
 def has_not_autocomplete(filename, selector):
-    """Check autocomplete attribute."""
+    """
+    Check the autocomplete attribute.
+
+    Check if tags ``form`` and ``input`` have the ``autocomplete``
+    attribute set to ``off``.
+
+    :param filename: Path to the ``HTML`` source.
+    :type filename: string
+    :param selector: CSS selector to test.
+    :type selector: string
+    """
     attr = 'autocomplete'
     value = 'off'
     has_attr = has_attribute(
@@ -58,9 +84,12 @@ def has_not_autocomplete(filename, selector):
 def is_cacheable(filename):
     """Check if cache is posible.
 
-    Verifies if the file (filename) has the tags
-    <META HTTP-EQUIV="Pragma" CONTENT="no-cache"> and
-    <META HTTP-EQUIV="Expires" CONTENT="-1">
+    Verifies if the file has the tags::
+       <META HTTP-EQUIV="Pragma" CONTENT="no-cache"> and
+       <META HTTP-EQUIV="Expires" CONTENT="-1">
+
+    :param filename: Path to the ``HTML`` source.
+    :type filename: string
     """
     selector = 'html'
     tag = 'meta'
@@ -70,7 +99,7 @@ def is_cacheable(filename):
     has_http_equiv = has_attribute(
         filename, selector, tag, attr, value)
 
-    if has_http_equiv is False:
+    if not has_http_equiv:
         result = True
         show_open('{} attribute in {}'.format(attr, filename),
                   details=dict(value=value))
@@ -81,7 +110,7 @@ def is_cacheable(filename):
     has_content = has_attribute(
         filename, selector, tag, attr, value)
 
-    if has_content is False:
+    if not has_content:
         result = True
         show_open('{} attribute in {}'.format(attr, filename),
                   details=dict(value=value))
@@ -92,7 +121,7 @@ def is_cacheable(filename):
     has_http_equiv = has_attribute(
         filename, selector, tag, attr, value)
 
-    if has_http_equiv is False:
+    if not has_http_equiv:
         result = True
         show_open('{} attribute in {}'.format(attr, filename),
                   details=dict(value=value))
@@ -103,7 +132,7 @@ def is_cacheable(filename):
     has_content = has_attribute(
         filename, selector, tag, attr, value)
 
-    if has_content is False:
+    if not has_content:
         result = True
         show_open('{} attribute in {}'.format(attr, filename),
                   details=dict(value=value))
