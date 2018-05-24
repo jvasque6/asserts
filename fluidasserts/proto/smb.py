8ba8064225f0a99866c891147eb16cc3f2a0fd34
R Alberto Ballestas <rballestas@fluidattacks.com>
2018-05-24 12:14:00 -0500

    Docs: fluidasserts.proto.smb
    Former-commit-id: dc825b03985c22e93c706797ea283b36a0b33d0d

--- fluidasserts/proto/smb.py
+++ fluidasserts/proto/smb.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-"""SMB check module."""
+"""
+SMB check module.
+
+This module allows to check SMB vulnerabilites.
+"""
 
 # standard imports
 from __future__ import absolute_import
 import socket
+from typing import Optional, Union
 
 # 3rd party imports
 from smb import SMBConnection
@@ -16,8 +21,22 @@ from fluidasserts import show_unknown
 from fluidasserts.utils.decorators import track
 
 
-def _smb_connect(server=None, user=None, password=None, domain='WORKGROUP'):
-    """Return a SMB connection handler."""
+Typconn = SMBConnection.SMBConnection
+
+
+def _smb_connect(server: Optional[str] = None,
+                 user: Optional[str] = None,
+                 password: Optional[str] = None,
+                 domain: str = 'WORKGROUP') -> Union[Typconn, bool]:
+    """
+    Return an SMB connection handler.
+
+    :param server: The NetBIOS machine name of the remote server.
+    :param user: Username to authenticate SMB connection.
+    :param password: Password for given user.
+    :param domain: The network domain/workgroup. Defaults to 'WORKGROUP'
+    :return: SMBConnection object if possible, otherwise False
+    """
     try:
         client_machine_name = 'assertspc'
         conn = SMBConnection.SMBConnection(user, password,
@@ -33,8 +52,14 @@ def _smb_connect(server=None, user=None, password=None, domain='WORKGROUP'):
 
 
 @track
-def has_dirlisting(share, *args, **kwargs):
-    """Check if a SMB share has dirlisting."""
+def has_dirlisting(share: str, *args, **kwargs) -> bool:
+    r"""
+    Check if an SMB share has dirlisting.
+
+    :param share: The name of the shared folder.
+    :param \*args: Optional arguments for SMB connect.
+    :param \*\*kwargs: Optional arguments for SMB connection.
+    """
     conn = _smb_connect(*args, **kwargs)
 
     if not conn:
@@ -62,8 +87,14 @@ def has_dirlisting(share, *args, **kwargs):
 
 
 @track
-def is_anonymous_enabled(server=None, domain='WORKGROUP'):
-    """Check if a SMB share has dirlisting."""
+def is_anonymous_enabled(server: str = None,
+                         domain: str = 'WORKGROUP') -> bool:
+    """
+    Check if anonymous login is possible over SMB.
+
+    :param server: The NetBIOS machine name of the remote server.
+    :param domain: The network domain/workgroup. Defaults to 'WORKGROUP'
+    """
     user = 'anonymous'
     password = ''
     conn = _smb_connect(server=server, user=user, password=password,
