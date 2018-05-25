eea8c5861a619ff552e2e9602e895219f462e645
Andres Roldan <aroldan@fluidattacks.com>
2018-05-25 10:41:20 -0500

    Added rebustness tests for x509 module
    Former-commit-id: 6f9e35056ad713f586a0e90654801c265186ea57

--- test/test_x509_close.py
+++ test/test_x509_close.py
@@ -19,6 +19,7 @@ import fluidasserts.utils.decorators
 # Constants
 fluidasserts.utils.decorators.UNITTEST = True
 SSL_PORT = 443
+NON_EXISTANT = '0.0.0.0'
 
 #
 # Closing tests
@@ -37,14 +38,22 @@ SSL_PORT = 443
 def test_cert_active_close(run_mock):
     """Certificado aun esta vigente?."""
     assert not x509.is_cert_inactive(run_mock)
+    assert not x509.is_cert_inactive(NON_EXISTANT)
 
 
 def test_cert_lifespan_safe_close(run_mock):
     """Vigencia del certificado es segura?."""
     assert not x509.is_cert_validity_lifespan_unsafe(run_mock)
+    assert not x509.is_cert_validity_lifespan_unsafe(NON_EXISTANT)
 
 
 def test_is_sha1_used_close(run_mock):
     """Presencia de SHA1 en los algoritmos de cifrado?."""
     assert not x509.is_sha1_used(run_mock, SSL_PORT)
-    assert not x509.is_sha1_used('0.0.0.0', SSL_PORT)
+    assert not x509.is_sha1_used(NON_EXISTANT, SSL_PORT)
+
+
+def test_is_md5_used_close(run_mock):
+    """Presencia de MD5 en los algoritmos de cifrado?."""
+    assert not x509.is_md5_used(run_mock, SSL_PORT)
+    assert not x509.is_md5_used(NON_EXISTANT, SSL_PORT)
