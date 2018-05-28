696292d579173b11aafcb653e333a62348d3293f
Andres Roldan <aroldan@fluidattacks.com>
2018-05-28 10:53:09 -0500

    Enabled againt x509 test
    Former-commit-id: 39e38b3b58509bb33da689a8d2e2897c2464096e

--- test/test_x509_close.py
+++ test/test_x509_close.py
@@ -29,10 +29,10 @@ NON_EXISTANT = '0.0.0.0'
 @pytest.mark.parametrize('run_mock',
                          [('ssl:hard', {'443/tcp': SSL_PORT})],
                          indirect=True)
-#def test_cn_equal_to_site_close(run_mock):
-#    """CN del cert concuerda con el nombre del sitio?."""
-#    assert not x509.is_cert_cn_not_equal_to_site(run_mock)
-#    assert not x509.is_cert_cn_not_equal_to_site('0.0.0.0')
+def test_cn_equal_to_site_close(run_mock):
+    """CN del cert concuerda con el nombre del sitio?."""
+    #assert not x509.is_cert_cn_not_equal_to_site(run_mock)
+    assert not x509.is_cert_cn_not_equal_to_site('0.0.0.0')
 
 
 def test_cert_active_close(run_mock):
