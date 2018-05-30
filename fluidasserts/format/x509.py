4a6a50867c9654521edd3cda255becf879b04a34
Andres Roldan <aroldan@fluidattacks.com>
2018-05-30 17:21:44 -0500

    Fixed cert days validity calculation
    Former-commit-id: 5c83feb9707af823df0ef9a4a1a4ae54536f49e2

--- fluidasserts/format/x509.py
+++ fluidasserts/format/x509.py
@@ -205,7 +205,6 @@ def is_cert_validity_lifespan_unsafe(site: str, port: int = PORT) -> bool:
     cert_validity = \
         cert_obj.not_valid_after - cert_obj.not_valid_before
 
-    cert_validity_days = max_validity_days - cert_validity.days
     if cert_validity.days <= max_validity_days:
         show_close('Certificate has a secure lifespan',
                    details=dict(site=site, port=port,
@@ -214,7 +213,7 @@ def is_cert_validity_lifespan_unsafe(site: str, port: int = PORT) -> bool:
                                 not_valid_after=cert_obj.not_valid_after.
                                 isoformat(),
                                 max_validity_days=max_validity_days,
-                                cert_validity_days=cert_validity_days))
+                                cert_validity_days=cert_validity.days))
         result = False
     else:
         show_open('Certificate has an insecure lifespan',
@@ -224,7 +223,7 @@ def is_cert_validity_lifespan_unsafe(site: str, port: int = PORT) -> bool:
                                not_valid_after=cert_obj.not_valid_after.
                                isoformat(),
                                max_validity_days=max_validity_days,
-                               cert_validity_days=cert_validity_days))
+                               cert_validity_days=cert_validity.days))
         result = True
     return result
 
