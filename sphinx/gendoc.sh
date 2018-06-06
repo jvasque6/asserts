9445ede52481bcea0361944eb3086f52017ffb62
R Alberto Ballestas <rballestas@fluidattacks.com>
2018-06-06 08:40:56 -0500

    CI: Add aux script to gen doc
    So version can be easily retrieved during deploy
    
    
    Former-commit-id: c08867c0bc6a1bb90de5b3ac97b5ec60064c9147

---
+++ sphinx/gendoc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env/bash
+
+mkdir -p public/
+sphinx-apidoc -efM fluidasserts -o sphinx/source
+VER=$(ls /builds/fluidsignal/asserts/build/dist/ |\
+      sed 's_fluidasserts-\|.zip__g')
+echo $VER
+sphinx-build -D version="v.$VER" -D release="v.$VER" \
+             -a sphinx/source/ public/
