084137b20ca6c00d51acd47b05921875924a3f54
Andres Roldan <aroldan@fluidattacks.com>
2018-06-12 14:30:35 -0500

    Use open tests cases in closing section with proxy to check ConnError handling.
    Added missing period
    
    
    Former-commit-id: edc9ea52e811ae21b50869cfa7d1637f22663029

--- test/test_sca.py
+++ test/test_sca.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Test methods of fluidasserts.sca packages"""
+"""Test methods of fluidasserts.sca packages."""
 
 # standard imports
 import io
@@ -60,9 +60,9 @@ def test_has_vulnerabilities_close():
 
     os.environ['http_proxy'] = 'https://0.0.0.0:8080'
     os.environ['https_proxy'] = 'https://0.0.0.0:8080'
-    assert not bower.has_vulnerabilities('jquery', version='3.0.0')
-    assert not chocolatey.has_vulnerabilities('python', version='3.7.0')
-    assert not maven.has_vulnerabilities('maven', version='5.0.0')
-    assert not npm.has_vulnerabilities('npm', version='10.0.0')
-    assert not nuget.has_vulnerabilities('jquery', version='10.0.0')
-    assert not pypi.has_vulnerabilities('pipasdiahsds')
+    assert not bower.has_vulnerabilities('jquery')
+    assert not chocolatey.has_vulnerabilities('python')
+    assert not maven.has_vulnerabilities('maven')
+    assert not npm.has_vulnerabilities('npm')
+    assert not nuget.has_vulnerabilities('jquery')
+    assert not pypi.has_vulnerabilities('pip')
