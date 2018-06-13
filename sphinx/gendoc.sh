38fdb796a8c876bf3c77a722e978526e16a6b189
R Alberto Ballestas <rballestas@fluidattacks.com>
2018-06-13 09:24:00 -0500

    Docs: reenable linkcheck and coverage builders
    - Wrap some long lines to 80 col
    
    
    Former-commit-id: e048e5c7ff817d1f8410cf5654202c54b084b7ae

--- sphinx/gendoc.sh
+++ sphinx/gendoc.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env/bash
 
-find sphinx/source/example/ -name '*.py' \
-  -exec sh -c "python3 {$1} | perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' > {$1}.out" x {} \;
+find sphinx/source/example/ -name '*.py' -exec sh -c \
+ "python3 {$1} | perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' > {$1}.out"\
+ x {} \;
 
 python sphinx/source/example/qstart-sqli-open.py | \
        perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' \
@@ -15,5 +16,5 @@ VER=$(ls /builds/fluidsignal/asserts/build/dist/ |\
       sed 's_fluidasserts-\|.zip__g')
 sphinx-build -D version="v.$VER" -D release="v.$VER" \
              -a sphinx/source/ public/
-#sphinx-build -b linkcheck sphinx/source public/review/
-#sphinx-build -b coverage  sphinx/source public/review/
+sphinx-build -b linkcheck sphinx/source public/review/
+sphinx-build -b coverage  sphinx/source public/review/
