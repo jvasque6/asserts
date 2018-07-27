36389f153dea7f19836cc23cd8257e937ba2b189
Andres Roldan <aroldan@fluidattacks.com>
2018-07-27 08:28:22 -0500

    Removed unused variables
    More unused variables removing
    
    Not be so aggresive with cleaning
    
    
    Former-commit-id: 7602df03c180d13b629d020695714aae392c92fa

--- local-integration.nix
+++ local-integration.nix
@@ -4,5 +4,5 @@
 DOCKER_PASS="set"
 DOCKER_USER="set"
 
-gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" --env "FA_LICENSE_KEY=$FA_LICENSE_KEY" --env "FA_USER_EMAIL=$FA_USER_EMAIL" build
-gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" --env "FA_LICENSE_KEY=$FA_LICENSE_KEY" --env "FA_USER_EMAIL=$FA_USER_EMAIL" test
+gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" build
+gitlab-ci-multi-runner exec docker --docker-privileged --docker-volumes /var/run/docker.sock:/var/run/docker.sock --env "DOCKER_USER=$DOCKER_USER" --env "DOCKER_PASS=$DOCKER_PASS" --env "PYPI_USER=$PYPI_USER" --env "PYPI_PASS=$PYPI_PASS" test
