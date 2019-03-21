b5fee2f54763751ab091ba391d0666d2cb6348a7
Andres Roldan <aroldan@fluidattacks.com>
2019-03-21 17:01:56 -0500

    fix(build): #546 updated dep

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     ],
     install_requires=[
         'configobj==5.0.6',          # fluidasserts
-        'typed-ast==1.2.0',          # fluidasserts
+        'typed-ast==1.3.1',          # fluidasserts
         'PyPDF2==1.26.0',            # fluidasserts.format.pdf
         'requests==2.21.0',          # fluidasserts.proto.http
         'certifi==2018.11.29',       # fluidasserts.proto.ssl
