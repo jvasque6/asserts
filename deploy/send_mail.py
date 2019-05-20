fa159c7ee61e2aafe6ad7cae9723b8c5e860707e
kamadoatfluid <kamado@fluidattacks.com>
2019-05-20 11:15:08 -0500

    feat(back): #584 deploy.send_email
    - refact some code to include pythonic syntax
    - refact some code to introduce mypy static typing
    - update the email according to the new template
    - update environment vars to the new template
    - update mailchimp template to remove fixed parameters
    
    Closes #584

--- deploy/send_mail.py
+++ deploy/send_mail.py
@@ -2,44 +2,61 @@
 """Send notification email."""
 
 import os
+import git
+import glob
 import ntpath
-from glob import glob
-from git import Repo
 import mandrill
 
-API_KEY = os.environ['MANDRILL_APIKEY']
+PROJECT = os.environ['PROJECT']
+PROJECT_URL = os.environ['PROJECT_URL']
+
+MANDRILL_APIKEY = os.environ['MANDRILL_APIKEY']
+MANDRILL_NEW_VERSION_EMAIL_TO = \
+    os.environ['MANDRILL_NEW_VERSION_EMAIL_TO'].split(',')
+
+CI_COMMIT_SHA = os.environ['CI_COMMIT_SHA']
+CI_COMMIT_BEFORE_SHA = os.environ['CI_COMMIT_BEFORE_SHA']
 
 
-def _get_changelog():
-    """Get message of last commit."""
-    repo = Repo(os.getcwd())
-    changelog = repo.git.log('-1', '--pretty=<b>%s</b>\n%b')
-    return changelog.replace("\n", "<br />\n")
+def _get_message() -> str:
+    """Get Summary and Author Name of commits."""
+    repo = git.Repo(os.getcwd())
+    message: str = repo.git.log(
+        CI_COMMIT_BEFORE_SHA + '...' + CI_COMMIT_SHA,
+        '--pretty=format:<b>%s</b>%n%bCommitted by: %aN%n')
+    return message.replace('\n', '<br/>\n')
 
 
-def _get_version():
+def _get_version() -> str:
     """Get version of last deploy."""
-    path_zip = glob('build/dist/*.zip')[0]
+    path_zip = glob.glob('build/dist/*.zip')[0]
     return ntpath.basename(path_zip)[13:-4]
 
 
-def send_mail(template_name, email_to, context):
+def send_mail(template_name: str, email_to, context, tags) -> None:
     """Send notification email."""
-    mandrill_client = mandrill.Mandrill(API_KEY)
+    mandrill_client = mandrill.Mandrill(MANDRILL_APIKEY)
     message = {
-        'to': [],
-        'global_merge_vars': []
+        'to': [
+            {'email': email} for email in email_to
+        ],
+        'global_merge_vars': [
+            {'name': key, 'content': value} for key, value in context.items()
+        ],
+        'tags': tags,
     }
-    for email in email_to:
-        message['to'].append({'email': email})
-
-    for key, value in context.items():
-        message['global_merge_vars'].append(
-            {'name': key, 'content': value}
-        )
     mandrill_client.messages.send_template(template_name, [], message)
 
 
-send_mail('assertsnewversionr', ["engineering@fluidattacks.com"],
-          context={'version': _get_version(),
-                   'changelog': _get_changelog()})
+send_mail(
+    template_name='new_version',
+    email_to=MANDRILL_NEW_VERSION_EMAIL_TO,
+    context={
+        'project': PROJECT,
+        'version': _get_version(),
+        'message': _get_message(),
+        'project_url': PROJECT_URL,
+    },
+    tags=[
+        'general',
+    ])
