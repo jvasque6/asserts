4f06088bd6f4439c4dc59e28067691b18d2b414a
Andres Roldan <aroldan@fluidattacks.com>
2019-07-15 19:17:13 -0500

    test(back): #0 fix tests
    - format.jks
    - format.pkcs

--- test/test_format_jks.py
+++ test/test_format_jks.py
@@ -25,19 +25,19 @@ NON_EXISTING_FILE = f'test/static/format/jks/does_not_exist.jks'
 #
 
 
-def has_no_password_protection_open():
+def test_has_no_password_protection_open():
     """Test if jks file is not password protected."""
     assert jks.has_no_password_protection(NO_PWD_DIR)
     assert jks.has_no_password_protection(NO_PWD_FILE)
 
 
-def use_password_open():
+def test_use_password_open():
     """Test if jks file is protected by a password from list."""
     assert jks.use_password(PWD_DIR, 'password123')
     assert jks.use_password(PWD_FILE, 'password123')
 
 
-def use_passwords_open():
+def test_use_passwords_open():
     """Test if jks file is protected by a password from list."""
     assert jks.use_passwords(PWD_DIR, ['password123', 'test'])
     assert jks.use_passwords(PWD_FILE, ['test', 'password123'])
@@ -48,20 +48,20 @@ def use_passwords_open():
 #
 
 
-def has_no_password_protection_close():
+def test_has_no_password_protection_close():
     """Test if jks file is password protected."""
     assert not jks.has_no_password_protection(PWD_DIR)
     assert not jks.has_no_password_protection(PWD_FILE)
     assert not jks.has_no_password_protection(NON_EXISTING_FILE)
 
 
-def use_password_close():
+def test_use_password_close():
     """Test if jks file is not protected by a password from list."""
     assert not jks.use_password(PWD_DIR, 'this-is-not-the-pass')
     assert not jks.use_password(PWD_FILE, 'this-is-not-the-pass')
 
 
-def use_passwords_close():
+def test_use_passwords_close():
     """Test if jks file is not protected by a password from list."""
     assert not jks.use_passwords(PWD_DIR, [])
     assert not jks.use_passwords(PWD_DIR, ['this-is-not-the-pass', ''])
@@ -74,13 +74,13 @@ def use_passwords_close():
 #
 
 
-def has_no_password_protection_unknown():
+def test_has_no_password_protection_unknown():
     """Test if jks file does not exist."""
     assert not jks.has_no_password_protection(NON_EXISTING_DIR)
     assert not jks.has_no_password_protection(NON_EXISTING_FILE)
 
 
-def use_password_unknown():
+def test_use_password_unknown():
     """Test if jks file does not exist."""
     assert not jks.use_password(NON_EXISTING_DIR, '')
     assert not jks.use_password(NON_EXISTING_DIR, 'password123')
@@ -88,12 +88,3 @@ def use_password_unknown():
     assert not jks.use_password(NON_EXISTING_FILE, '')
     assert not jks.use_password(NON_EXISTING_FILE, 'password123')
     assert not jks.use_password(NON_EXISTING_FILE, 'this-is-not-the-pass')
-
-def use_passwords_unknown():
-    """Test if jks file does not exist."""
-    assert not jks.use_passwords(NON_EXISTING_DIR, [])
-    assert not jks.use_passwords(NON_EXISTING_DIR, ['password123'])
-    assert not jks.use_passwords(NON_EXISTING_DIR, ['this-is-not-the-pass'])
-    assert not jks.use_passwords(NON_EXISTING_FILE, [])
-    assert not jks.use_passwords(NON_EXISTING_FILE, ['password123'])
-    assert not jks.use_passwords(NON_EXISTING_FILE, ['this-is-not-the-pass'])
--- test/test_format_pkcs12.py
+++ test/test_format_pkcs12.py
@@ -22,7 +22,7 @@ PWD_FILE = 'test/static/format/pkcs12/pwd.p12'
 #
 
 
-def has_no_password_protection_open():
+def test_has_no_password_protection_open():
     """p12 file is not password protected."""
     assert pkcs12.has_no_password_protection(NO_PWD_FILE)
 
@@ -32,7 +32,7 @@ def has_no_password_protection_open():
 #
 
 
-def has_no_password_protection_close():
+def test_has_no_password_protection_close():
     """p12 file is password protected or file does not exist."""
     assert not pkcs12.has_no_password_protection(PWD_FILE)
     assert not pkcs12.has_no_password_protection(NON_EXISTING_FILE)
